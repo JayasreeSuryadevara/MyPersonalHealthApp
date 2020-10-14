3558a04c9d4c9297722ce98943aed4226b03d2f8
JayasreeSuryadevara <sjayas@hotmail.com>
2020-10-14 12:02:48 -0700

    Create Express Route Tasks

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Import routes defined for the app
+const tasks = require("./routes/api/tasks");
+
+// Use the imported routes
+app.use("/api/tasks", tasks);
+
 // Connect to MongoDB using Mongoose
 mongoose
   .connect(db, { useUnifiedTopology: true })
@@ -23,12 +29,6 @@ mongoose
 
 // Create a basic route
 app.get("/", (req, res) => res.send("Welcome to your own health tracker"));
-
-// Import routes defined for the app
-const tasks = require("./routes/api/tasks");
-
-// Use the imported routes
-app.use("/api/tasks", tasks);
 
 // Assign a port for the app to run on
 const port = process.env.PORT || 5000;
--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -2,6 +2,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+router.get("/tasks", (req, res) => res.json({ msg: "This is the tasks route" }));
 
 module.exports = router;
